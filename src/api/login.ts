fef7728d4a06bb70d098992db0cfc8cf5521fed0
June <zkitun@163.com>
2021-03-24 11:29:41 +0800

    添加CSS预处理器less

--- src/api/login.ts
+++ src/api/login.ts
@@ -1,6 +1,10 @@
 import http from '@/utils/http';
+interface LoginData {
+  username: string,
+  password: string
+}
 
-const login = (data : any) => {
+const login = (data : LoginData | null) => {
   return http.request({
     url: '/login',
     method: 'post',
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,36 +30,60 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div class="hw-less">
+      <h1>测试less</h1>
+      <div></div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
 
-@Options({
-  props: {
-    msg: String
-  }
-})
+  @Options({
+    props: {
+      msg: String
+    }
+  })
 export default class HelloWorld extends Vue {
-  msg!: string
+    msg!: string
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="less">
+  h3 {
+    margin: 40px 0 0;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .hw-less {
+    h1 {
+      color: aquamarine;
+    }
+
+    div {
+      width: 300px;
+      height: 300px;
+      background: #42b983;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      margin-left: -150px;
+      margin-top: -150px;
+    }
+  }
 </style>
