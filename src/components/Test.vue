0fda923c61f871074f8c147646f8e16355dbc281
June <zkitun@163.com>
2021-03-24 17:14:31 +0800

    添加Vue3 Composition API风格代码示例

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -10,12 +10,14 @@
     <van-button @click="dec">-1</van-button>
     <van-button @click="set(12)">设置</van-button>
     <van-button @click="reset">重置</van-button>
+    <hr>
+    <van-button @click="showToast">轻提示</van-button>
   </div>
 </template>
 
 <script lang="ts">
 import { reactive, computed } from 'vue';
-import { Button } from 'vant';
+import { Button, Toast } from 'vant';
 import useCount from '@/components/useCount';
 
 export default {
@@ -25,7 +27,6 @@ export default {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   setup() {
     const { current, inc, dec, set, reset } = useCount(100);
-    console.log(current.value);
     const state: any = reactive({
       count: 4,
       double: computed(() => (state.count * 2))
@@ -35,9 +36,17 @@ export default {
       state.count++;
     };
 
+    const showToast = () => {
+      Toast.loading({
+        message: '加载中...',
+        forbidClick: true
+      });
+    };
+
     return {
       state,
       increment,
+      showToast,
       current,
       inc,
       dec,
